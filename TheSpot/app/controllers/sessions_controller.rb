bd382f74eb5ea896b2b447c02c34b37a5d6b1689
chris wallwork <chris.wallwork@gmail.com>
2014-10-10 17:29:28 -0400

    user login

--- TheSpot/app/controllers/sessions_controller.rb
+++ TheSpot/app/controllers/sessions_controller.rb
@@ -1,6 +1,25 @@
 class SessionsController < ApplicationController
 
-  # def new
+  def new
 
-  # end
+  end
+
+  def create
+    @user = User.find_by(username: sessions_params[:username])
+    .try(:authenticate, sessions_params[:password])
+
+    if @user
+      session[:user_id] = @user.id
+      redirect_to login_path  # BUGBUG redirect spot_index_path? (route for spots not yet defined)
+    else
+      flash[:login] = "Invalid Credentials"
+      redirect_to login_path
+    end
+  end
+
+  private
+
+  def sessions_params
+    params.require(:session).permit(:username, :password)
+  end
 end
