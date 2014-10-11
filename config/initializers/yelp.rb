60a9d71a9d8e3809cb78e12108b500c59b1c2720
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-11 17:01:32 -0400

    add dotenv gem to Gemfile

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,6 +1,8 @@
-@yelp_client = Yelp.client.configure do |config|
-  config.consumer_key = ENV[CONSUMER_KEY]
-  config.consumer_secret = ENV[CONSUMER_SECRET]
-  config.token = ENV[TOKEN]
-  config.token_secret = ENV[TOKEN_SECRET]
+require 'yelp'
+
+Yelp.client.configure do |config|
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.token = ENV['TOKEN']
+  config.token_secret = ENV['TOKEN_SECRET']
 end
