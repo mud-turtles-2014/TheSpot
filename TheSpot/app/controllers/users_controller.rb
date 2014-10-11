e231e33e4a3ca90d71683bf0e77902f6a8324889
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-11 12:02:07 -0400

    Add user#update

--- TheSpot/app/controllers/users_controller.rb
+++ TheSpot/app/controllers/users_controller.rb
@@ -18,6 +18,15 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def update
+  	@user = User.find(params[:id])
+  	  if @user.update(user_params)
+  	  	redirect_to user_path(@user)
+  	  else
+  	  	redirect_to edit_user_path(@user.id)
+  	  end
+  end
+
   def user_params 
   	params.require(:user).permit([:email, :username, :password, :password_confirmation])
   end
