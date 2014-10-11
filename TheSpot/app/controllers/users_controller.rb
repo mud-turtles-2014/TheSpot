fd3ec74f6f028d0269ba602cc858c60f5484c3f0
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-11 11:22:42 -0400

    Add strong params to user_controller

--- TheSpot/app/controllers/users_controller.rb
+++ TheSpot/app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
+  def user_params 
+  	params.require(:user).permit([:email, :username, :password, :password_confirmation])
+  end
 end
