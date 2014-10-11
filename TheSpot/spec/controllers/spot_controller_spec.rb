fa306def7a4e289fa08fcac6d41424d23bea3328
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-11 15:48:21 -0400

    Add spec file for spots_controller

---
+++ TheSpot/spec/controllers/spot_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe SpotsController do
+  describe "#new" do
+    it "assigns a new spot to @spot"
+    it "renders the #new template"
+  end
+
+  describe "#create" do
+    context "with valid attributes" do
+      it "saves the new spot to the database"
+      it "redirects to the #show page"
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new spot to the database"
+      it "redirects to the #new page"
+      it "assigns a flash to notify user of error"
+    end
+  end
+
+  describe "#show" do
+    it "assigns the spot to @spot"
+    it "renders the show template"
+  end
+
+  describe "#edit" do
+    it "assigns the spot to @spot"
+    it "renders the :edit template"
+    end
+  end
+
+  describe "#update" do 
+  	context "with valid attributes" do 
+  	  it "updates the database"
+  	  it "redirects to spot#show"
+  	end
+
+  	context "with invalid attributes" do 
+  	  it "does not update the database"
+  	  it "redirects to spot#edit"
+  	end
+  end
+
+  describe "#destroy" do 
+    it "deletes the spot from the database"
+    it "redirects to spot index"
+  end
+end
