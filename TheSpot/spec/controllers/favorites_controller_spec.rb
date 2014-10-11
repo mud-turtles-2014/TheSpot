b6352d19b90442de100fd11e3eeb52497e4f9498
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-11 11:32:18 -0400

    add files, Factory Girl settings to rails_helper

---
+++ TheSpot/spec/controllers/favorites_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe FavoritesController do
+  describe "POST #create" do
+    context "with valid attributes" do
+    end
+    context "with invalid attributes" do
+    end
+  end
+end
