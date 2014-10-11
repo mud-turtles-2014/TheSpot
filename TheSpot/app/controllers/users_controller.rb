5170ed1f9ae249b359e90448040182c8ecbc5e5c
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-11 11:24:58 -0400

    Add users#create

--- TheSpot/app/controllers/users_controller.rb
+++ TheSpot/app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      redirect_to '/login'
+    else
+      redirect_to new_user_path
+    end
+  end
+
   def user_params 
   	params.require(:user).permit([:email, :username, :password, :password_confirmation])
   end
