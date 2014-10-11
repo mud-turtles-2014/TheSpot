4cc0459bc798ae689a1bc866cfae6f72fcfd2097
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-11 11:42:34 -0400

    Add route for users#edit

--- TheSpot/config/routes.rb
+++ TheSpot/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :sessions
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
