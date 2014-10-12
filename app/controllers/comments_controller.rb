61fb950b063a3b6aa3ab1dcda74baed001d64b95
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-12 00:25:24 -0400

    Add #new method and modify #create for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,27 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.create(user_id: params[:user_id], spot_id: params[:spot_id], body: params[:body])
-    respond_to do |format|
-      format.html { redirect_to spots_path }
-      format.json { render json: @comment.to_json }
+    @comment = Comment.new(comment_params)
+    # change user_id to grab from current user
+    @comment.assign_attributes(user_id: 1, spot_id: params[:spot_id])
+    if @comment.save
+      redirect_to spot_path(params[:spot_id])
     end
+
+    # respond_to do |format|
+    #   format.html { redirect_to spots_path }
+    #   format.json { render json: @comment.to_json }
+    # end
+  end
+
+  def new
+  	@spot = Spot.find(params[:spot_id])
+  	@comment = Comment.new
+  end
+
+  private
+
+  def comment_params
+  	params.require(:comment).permit([:body])
   end
 end
