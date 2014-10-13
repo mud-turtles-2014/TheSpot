7d31a1cec9b522dd06813d1c9d8941ee2d58fef8
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-12 23:03:46 -0400

    Add CSS styling

--- app/helpers/spots_helper.rb
+++ app/helpers/spots_helper.rb
@@ -1,17 +1,11 @@
 module SpotsHelper
   def show_photo(url)
-  	("<img src='" + url + "' class='spot-photo'/>").html_safe
+  	("<div class='spot-photo'><img src='" + url + "'/></div>").html_safe
   end
 
   def show_price(count)
   	string = ""
   	count.times { string += "$"}
   	string.html_safe
-  end
-
-  def format_phone(phone)
-  	if phone.length == 9
-  	  "212-123-1234".html_safe
-  	end
   end
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,4 +3,8 @@ class Spot < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :favorites
+
+  def is_favorited_by(user)
+  	self.favorites.include?(user.favorite)
+  end
 end
