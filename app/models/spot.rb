f48af8989e9f059dc27d5b5aee15783808e21c33
steph t. zhang <stephtzhang@gmail.com>
2014-10-14 12:28:41 -0400

    Add geocode to model and controller

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,7 +3,10 @@ class Spot < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :favorites
-  
+
+  geocoded_by :address
+  after_validation: geocode
+
   def is_favorited_by(user)
   	self.favorites.include?(user.favorite)
   end
