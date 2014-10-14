cb0828c658f342a86d351490a5abfe52063726fe
steph t. zhang <stephtzhang@gmail.com>
2014-10-14 12:25:38 -0400

    Add latitude and longitude to spots table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141013114503) do
+ActiveRecord::Schema.define(version: 20141014161933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20141013114503) do
     t.integer  "favorites_count", default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "users", force: true do |t|
